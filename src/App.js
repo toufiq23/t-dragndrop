f577df089abd4819fc0cb175c40309355c3a64a0
toufiq23 <drrameraaron023@gmail.com>
2021-10-11 11:10:23 +0200

    t-dragndrop app created using pure react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import "./App.css"
+
+const initList = [1, 2, 3, 4, 5, 6]
 
 function App() {
+	const [list, setList] = useState(initList)
+	const [draggedItem, setDraggedItem] = useState(null)
+
+	function onDragStartHandle(e, index){
+		setDraggedItem(list[index])
+		e.dataTransfer.effectAllowed = "move"
+		e.dataTransfer.setData('text/html', e.target.parentNode)
+		e.dataTransfer.setDragImage(e.target.parentNode, 20, 20)
+	}
+
+	function onDragOverHandle(index){
+		const draggedOverItem = list[index]
+
+		if(draggedOverItem === draggedItem){
+			return
+		}
+
+		const items = list.filter(item => item !== draggedItem)
+
+		items.splice(index, 0, draggedItem)
+		setList(items)
+	}
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+				<h3>Drag and Drop Project</h3>
+				<ul>
+					{
+						list.map((item, idx) => {
+							return (
+								<li key={idx} onDragOver={() => onDragOverHandle(idx)} className="item-style">			<div draggable onDragStart={(e) => onDragStartHandle(e, idx)}>{item}</div>
+								</li>
+							)
+						})
+					}
+				</ul>
+
+				<div className="footer">
+					<div className="container">
+						<h2>Thanks for your visit.</h2>
+						<h3>Created by M Toufiq ELAHI</h3>
+					</div> 
+				</div>
+			</header>
     </div>
   );
 }
